cff022a7f35ecc134640d1a9bff5962868d15439
Pranay Kekre <49832626+PranayK27@users.noreply.github.com>
2020-01-05 13:22:50 +0000

    Add files via upload

--- src/app/search-bovine/search-bovine.component.ts
+++ src/app/search-bovine/search-bovine.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Bovine } from '../bovine';
 import { BovineService } from '../bovine-service.service';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-search-bovine',
@@ -9,21 +11,62 @@ import { BovineService } from '../bovine-service.service';
 })
 export class SearchBovineComponent implements OnInit {
 
-  id: number;
-  bovine: Bovine[];
+  searchForm: FormGroup;
+  searchCriteria: SearchBovinesForm;
+  // id: number;
+   bovine: Bovine[];
 
   constructor(private dataService: BovineService) { }
 
-  ngOnInit() {
-    // this.id = 0;
+  ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      movedFrom: new FormControl(null, []),
+      movedTo: new FormControl(null, []),
+      thirdParty: new FormControl(null, []),
+      tagNumber: new FormControl(null, []),
+      healthCertNo: new FormControl(null, []),
+      fromDate: new FormControl(null, []),
+      toDate: new FormControl(null, []),
+      provinceMovementSyastem: new FormControl(null, []),
+    });
+  }
+
+// isFormInvalid() {
+//     return false;
+//   }
+
+submitSearchForm(): void {
+    const search: SearchBovinesForm = {
+      movedFrom: this.searchForm.controls.movedFrom.value,
+      movedTo: this.searchForm.controls.movedTo.value,
+      thirdParty: this.searchForm.controls.thirdParty.value,
+      tagNumber: this.searchForm.controls.tagNumber.value,
+      healthCertNo: this.searchForm.controls.healthCertNo.value,
+      fromDate: this.searchForm.controls.fromDate.value,
+      toDate: this.searchForm.controls.toDate.value,
+      provinceMovementSystem: this.searchForm.controls.provinceMovementSystem.value
+    };
+    this.searchCriteria = search;
   }
 
   searchBovine() {
-    this.dataService.getBovineById(this.id)
+    this.dataService.getBovineByTagNumber(this.searchForm.controls.tagNumber.value)
       .subscribe(bovine => this.bovine = bovine);
   }
 
   onSubmit() {
+    //this.submitted = true;
     this.searchBovine();
   }
 }
+export class SearchBovinesForm {
+  movedFrom: string;
+  movedTo: string;
+  thirdParty: string;
+  tagNumber: string;
+  healthCertNo: string;
+  fromDate: Date;
+  toDate: Date;
+  provinceMovementSystem: string;
+}
+
